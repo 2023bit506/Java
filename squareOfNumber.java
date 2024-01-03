0b721487759e092865cb0b32ccc5bea81f7afd20
2023bit506 <2023bit506@sggs.ac.in>
2024-01-03 08:33:04 +0530

    Square of number

---
+++ squareOfNumber.java
@@ -0,0 +1,12 @@
+public class squareOfNumber {
+    public static void main(String[] args)
+    {
+        int n = 5;
+        int result;
+        for(int i=1;i<=10;i++)
+        {
+            result = n*i;
+            System.out.println(result);
+        }
+    }
+}

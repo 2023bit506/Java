58479d1e0621c32d78b03d07efa0b2f76b645dd9
2023bit506 <2023bit506@sggs.ac.in>
2024-01-12 09:34:35 +0530

    Return Types

---
+++ returnTypesInJava.java
@@ -0,0 +1,68 @@
+public class returnTypesInJava {
+    public static void main(String[] args) {
+        returnVoidValue();
+        System.out.println(returnBooleanValue());
+        System.out.println(returnCharValue());
+        System.out.println(returnByteValue());
+        System.out.println(returnShortValue());
+        System.out.println(returnIntValue());
+        System.out.println(returnLongValue());
+        System.out.println(returnFloatValue());
+        System.out.println(returnDoubleValue());
+        System.out.println(returnStringValue());
+        
+    }
+
+    public static void returnVoidValue()
+    {
+        System.out.println("Shubham Pradip Pawar");
+    }
+
+    public static boolean returnBooleanValue()
+    {
+        return true;
+    }
+
+    public static char returnCharValue()
+    {
+        return 'a';
+    }
+
+    public static byte returnByteValue()
+    {
+        return 118;
+    }
+
+    public static short returnShortValue()
+    {
+        return 32456;
+    }
+
+    public static int returnIntValue()
+    {
+        return 10;
+    }
+
+    public static long returnLongValue()
+    {
+        return 111114545l;
+    }
+
+    public static float returnFloatValue()
+    {
+        return 11.4f;
+    }
+
+    public static double returnDoubleValue()
+    {
+        return 11.44;
+    }
+
+    public static String returnStringValue()
+    {
+        return "Hello";
+    }
+
+ 
+
+}

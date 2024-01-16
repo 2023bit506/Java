194c46cdfb746e10c8fc457e442459501bb73c5a
2023bit506 <2023bit506@sggs.ac.in>
2024-01-16 06:41:26 +0530

    Return Zero

---
+++ returnZero.java
@@ -0,0 +1,43 @@
+// public class returnZero
+// {
+//     public static int returnZero1(){
+//         return 15;
+//     }
+
+//     public static int Add()
+//     {
+//         int a = 10;
+//         int b = 20;
+//         returnZero1();
+//         return a+b;
+//         // System.out.println(a+b);
+//     }
+//     public static void main(String[] args) 
+//     {
+//         System.out.println(returnZero1());
+//         System.out.println(Add());
+//         // Add();
+//     }
+// }
+// public class returnZero
+// {
+//     private returnZero()
+//     {
+//         System.out.println("HEYYY");
+//     }
+//     public static void main(String[] args) {
+//         returnZero z1 = new returnZero();
+
+//     }
+// }
+
+public class returnZero
+{
+    int a;
+    public static void main(String[] args) {
+        // returnZero z1 = new returnZero();
+        int a;
+        // System.out.println(z1.a);
+        System.out.println(a);
+    }
+}

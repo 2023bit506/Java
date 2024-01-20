41700848b1b48a0b3d2fd8703c371da51689462f
2023bit506 <2023bit506@sggs.ac.in>
2024-01-20 08:46:38 +0530

    String Program

---
+++ strings.java
@@ -0,0 +1,14 @@
+public class strings
+{
+    public static void main(String[] args)
+    {
+        String s1 = "Shubham"; //creating string by java string literal
+        char ch[] = {'P','a','w','a','r'};
+        String s2 = new String(ch);  //converting character array to string.
+        String s3 = new String("2023bit506"); //creating java string by new keyword.
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+}
